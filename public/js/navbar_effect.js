3b3d9a84e109b97121644978c300cb7af7a48ca6
ntut_oop_ta <ntut_oop_ta@gmail.com>
2020-04-27 19:11:06 +0800

    index finish

--- public/js/navbar_effect.js
+++ public/js/navbar_effect.js
@@ -1,9 +1,9 @@
 window.addEventListener('scroll', function(e) {
   var vscroll = window.scrollY;
   var navbar = document.getElementById('header');
-  if(vscroll<700){
-    navbar.style.cssText="background-color: RGBA(9,9,17,0.3);"
+  if(vscroll<300){
+    navbar.style.cssText="opacity: 0;"
   }else{
-    navbar.style.cssText="background-color: RGB(9,9,17);"
+    navbar.style.cssText="opacity: 1;"
   }
 });
