ee662389a620d113d81ccd0c30af48d0a7dbb961
ntut_oop_ta <ntut_oop_ta@gmail.com>
2020-06-25 21:37:32 +0800

    rwd add a little

--- js/navbar_effect.js
+++ js/navbar_effect.js
@@ -2,8 +2,8 @@ window.addEventListener('scroll', function(e) {
   var vscroll = window.scrollY;
   var navbar = document.getElementById('header');
   if(vscroll<300){
-    navbar.style.cssText="opacity: 0;"
+    navbar.style.cssText="opacity: 0;";
   }else{
-    navbar.style.cssText="opacity: 1;"
+    navbar.style.cssText="opacity: 1;";
   }
 });
